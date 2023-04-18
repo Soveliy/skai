a3622f3133704beec0a400de9a3391c745f495fe
Soveliy <nikita.elagin2012@yandex.ru>
2023-04-18 10:47:38 +0300

    added page

---
+++ src/assets/scripts/blocks/main.js
@@ -0,0 +1,292 @@
+let swiper = new Swiper(".prodTizer__slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop:true,
+    autoplay: {
+      delay: 5000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  });
+
+
+  let tl = gsap.timeline()
+  let tl2 = gsap.timeline()
+  let tl3 = gsap.timeline()
+  let tl4 = gsap.timeline()
+  let logoAbout = gsap.timeline()
+
+  logoAbout.to(".aboutProd__logo", 
+  {
+    x: "250%",
+
+    scrollTrigger:{
+      // duration:1,
+      trigger:".aboutProd__item--orange",
+      scrub:true,
+      start:'top center',
+      end:"+=1800",
+       ease: "none",
+    }
+  });
+
+
+  tl2.to(".newMethod__logo", 
+  {
+    x: "250%",
+
+    scrollTrigger:{
+      // duration:1,
+      trigger:".newMethod",
+      scrub:true,
+      start:'top center',
+      end:"+=1800",
+       ease: "none",
+    }
+  });
+  ScrollTrigger.matchMedia({
+    "(min-width: 320px) and (max-width: 1023px)": function minWidth320pxAndMaxWidth599px() {
+      tl.to(".aboutProd__item--pink", 
+      {
+        backgroundImage: "linear-gradient(180deg, rgba(254,210,51,1) 0%, rgba(253,155,36,1) 100%)",
+        clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".aboutProd__item--pink",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          // ease: "none",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--1"}
+      
+        }
+      });
+    
+      tl2.to(".aboutProd__item--blue", 
+      {
+        backgroundImage: " linear-gradient(90deg, #77BBE8 0%, #C7A4EB 52.55%, #FF5238 100%)",
+        clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".aboutProd__item--blue",
+          scrub:true,
+          start:'top center',
+          end:"+=50",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--2"}
+         
+        }
+      });
+      tl3.to(".aboutProd__item--purple", 
+      {
+        backgroundImage: "linear-gradient(90deg, #CFA4F3 0%, #FBB28E 100%)",
+        clearProps:true,
+        scrollTrigger:{
+      
+          trigger:".aboutProd__item--purple",
+          scrub:true,
+          start:'top center',
+          end:"+=50",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--3"}
+        }
+      });
+      
+      tl4.to(".aboutProd__item--orange", 
+      {
+        backgroundImage: "linear-gradient(90deg, #F9CF0A 0%, #F73C59 100%)",
+        clearProps:true,
+        scrollTrigger:{
+    
+          trigger:".aboutProd__item--orange",
+          scrub:true,
+          start:'top center',
+          end:"+=50",
+         
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--4"}
+        }
+      });
+      
+      tl.to(".newMethod__item--mask-1", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-1",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-1", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+    
+      tl2.to(".newMethod__item--mask-2", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-2",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-2", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+      tl3.to(".newMethod__item--mask-3", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-3",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-3", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+     
+    },
+
+    "(min-width: 1024px)": function minWidth1365pxAndMaxWidth2565px() {
+      tl.to(".aboutProd__item--pink", 
+      {
+        backgroundImage: "linear-gradient(180deg, rgba(254,210,51,1) 0%, rgba(253,155,36,1) 100%)",
+        clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".aboutProd__item--pink",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          // ease: "none",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--1"}
+      
+        }
+      });
+    
+      tl2.to(".aboutProd__item--blue", 
+      {
+        backgroundImage: " linear-gradient(90deg, #77BBE8 0%, #C7A4EB 52.55%, #FF5238 100%)",
+        clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".aboutProd__item--blue",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--2"}
+         
+        }
+      });
+      tl3.to(".aboutProd__item--purple", 
+      {
+        backgroundImage: "linear-gradient(90deg, #CFA4F3 0%, #FBB28E 100%)",
+        clearProps:true,
+        scrollTrigger:{
+      
+          trigger:".aboutProd__item--purple",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--3"}
+        }
+      });
+      
+      tl4.to(".aboutProd__item--orange", 
+      {
+        backgroundImage: "linear-gradient(90deg, #F9CF0A 0%, #F73C59 100%)",
+        clearProps:true,
+        scrollTrigger:{
+    
+          trigger:".aboutProd__item--orange",
+          scrub:true,
+          start:'top center',
+          end:"+=100",
+         
+          toggleClass: {targets: ".aboutProd__images-list", className: "aboutProd__images-list--4"}
+        }
+      });
+      
+    
+   
+    
+      //  Вариант с пином элементов
+      // let st = ScrollTrigger.create({
+      //   trigger: ".newMethod__list",
+      //   pin: ".newMethod__list",
+      //   start: "top center",
+      //   end: "+=300"
+      // });
+    
+      tl.to(".newMethod__item--mask-1", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-1",
+          scrub:true,
+          start:'top-=300px center',
+          end:"center-80px center",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-1", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+    
+      tl2.to(".newMethod__item--mask-2", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-2",
+          scrub:true,
+          start:'top center',
+          end:"center+=100px center",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-2", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+      tl3.to(".newMethod__item--mask-3", 
+      {
+        // backgroundColor: "#F5F5F7",
+        // clearProps:true,
+        scrollTrigger:{
+       
+          trigger:".newMethod__item--mask-3",
+          scrub:true,
+          start:'center+=101px center',
+          end:"center+=250px center",
+          // markers:true,
+          // ease: "none",
+          toggleClass: {targets: ".newMethod__item--mask-3", className: "newMethod__item--isCurrent"}
+         
+        }
+      });
+    
+    
+    
+
+      
+     
+    },
+ 
+  });
---
+++ src/assets/scripts/script.js
@@ -0,0 +1,15 @@
+// script.js
+
+// Script
+
+// Plugins
+
+@@include('./plugins/swiper-bundle.min.js');
+@@include('./plugins/gsap.min.js');
+@@include('./plugins/ScrollTrigger.min.js');
+// Blocks
+
+document.addEventListener('DOMContentLoaded', () => {
+   @@include('./blocks/main.js');
+
+});
